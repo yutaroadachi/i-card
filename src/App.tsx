c679d08f8e818c2dfa57bbf934093ee4fb077654
yutaroadachi <ytr_adc72@yahoo.co.jp>
2020-02-02 20:56:43 +0900

    feat: styled-componentsを導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const App = () => {
-  return <h1>Hello World!</h1>;
+  const HelloWorld = styled.h1`
+    color: red;
+  `;
+
+  return <HelloWorld>Hello World!</HelloWorld>;
 };
 
 export default App;
